42e6d3549f41611b07bfd8ca50706b571159bd9d
foofsi <34344240+foofsi@users.noreply.github.com>
2017-12-08 17:40:11 +0300

    display it boy

--- select.php
+++ select.php
@@ -4,4 +4,21 @@
 
 $res = $mysqli->query("SELECT * FROM users");
 
+
+for ($row_no = $res->num_rows - 1; $row_no >= 0; $row_no--) {
+    $res->data_seek($row_no);
+    $row = $res->fetch_assoc();
+    //echo " id = " . $row['ID'] . "\n";
+    echo "<tr>";
+    echo "<td>".$row["ID"]."</td>";
+    echo "<td>".$row["First_Name"]."</td>";
+    echo "<td>".$row["Last_Name"]."</td>";
+    echo "<td>".$row["Phone"]."</td>";
+    echo "<td>".$row["Username"]."</td>";
+    echo "<td>".$row["Password"]."</td>";
+    echo "</tr>";
+  }
+
+
+
 ?>
