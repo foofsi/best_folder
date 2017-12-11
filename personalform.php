6c9a56b8f1eace2fb79ec4cf7029808f188aa439
foofsi <34344240+foofsi@users.noreply.github.com>
2017-12-11 16:40:03 +0300

    new file

---
+++ personalform.php
@@ -0,0 +1,30 @@
+<?php
+
+ require_once("connect.php");
+
+?>
+
+<?php
+if(isset($_POST['First_Name'], $_POST['Last_Name'], $_POST['Phone'], $_POST ['Username'], $_POST ['Password'] ))
+
+{
+  $First_Name= $_POST['First Name'];
+  $Last_Name= $_POST['Last Name'];
+  $Phone= $_POST['Phone'];
+  $Username= $_POST['Username'];
+  $Password= $_POST['Password'];
+
+  echo "First Name: {$First_Name}  Last Name: {$Last_Name} Phone Number: {$Phone} Username: {$Username} Password: {$Password}";
+
+}
+
+?>
+
+<form action = "index.php" method "post">
+  <input type="text" name="First_Name" placeholder="First Name">
+  <input type="text" name="Last_Name" placeholder="Last Name">
+  <input type="text" name="Phone" placeholder="Phone Number">
+  <input type="text" name="Username" placeholder="Username">
+  <input type="text" name="Password" placeholder="Password">
+  <input type="submit">
+</form>
