0ae066637ae46ad77ed7f8120b9fd9ef8e9d39df
foofsi <34344240+foofsi@users.noreply.github.com>
2017-12-14 13:20:17 +0300

    now it works
    i fixed the INSERT INTO query
    
    added parantheses to the if statement
    
    and I added semi colons everywhere.

--- personalform.php
+++ personalform.php
@@ -5,20 +5,24 @@
 ?>
 
 <?php
-if(isset($_POST['First_Name']) && (isset($_POST['Last_Name']) && (isset($_POST['Phone']) && (isset($_POST['Username']) && (isset($_POST['Password']))
+if(isset($_POST['First_Name'])
+ && (isset($_POST['Last_Name']))
+  && (isset($_POST['Phone']))
+   && (isset($_POST['Username']))
+    && (isset($_POST['Password'])))
 
 
  {
 
-  $First_Name= $_POST['First_Name']
-  $Last_Name= $_POST['Last_Name']
-  $Phone= $_POST['Phone']
-  $Username= $_POST['Username']
-  $Password= $_POST['Password']
+  $First_Name= $_POST['First_Name'];
+  $Last_Name= $_POST['Last_Name'];
+  $Phone= $_POST['Phone'];
+  $Username= $_POST['Username'];
+  $Password= $_POST['Password'];
 
   echo "First Name: {$First_Name}  Last Name: {$Last_Name} Phone Number: {$Phone} Username: {$Username} Password: {$Password}";
 
-  $mysqli->query('INSERT INTO `users`(`First_Name`, `Last_Name`, `Phone`, `Username`, `Password`) VALUES ("'.$First_Name.'", "'.$Last_Name.'", "'.$Phone.'", "'.$Username.'", "'.$Password.'"');
+  $mysqli->query('INSERT INTO `users`(`First_Name`, `Last_Name`, `Phone`, `Username`, `Password`) VALUES ("'.$First_Name.'", "'.$Last_Name.'", "'.$Phone.'", "'.$Username.'", "'.$Password.'")');
 
 }
 
