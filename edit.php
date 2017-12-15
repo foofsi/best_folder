d5cc8b956a26022f6bb41abbb9fe0b68b2829d8f
foofsi <34344240+foofsi@users.noreply.github.com>
2017-12-15 12:26:45 +0300

    help on line 38
    i changed the edit file
    i feel like there is an error with the apostrophe/brackets
    Im also not sure what to put for WHERE in line 38

--- edit.php
+++ edit.php
@@ -4,18 +4,40 @@
 
 // check if the id is set by using $_GET[x], and then assign that to a variable (refer to delete.php)
 
-//$mysqli->query('DELETE FROM `users` WHERE  ID='.$ID); and assign that to $res (refer to index.php)
+if(isset($_GET['id']))
+{
+$ID= $_GET['id'];
+}
+
+// $mysqli->query('DELETE FROM `users` WHERE  ID='.$ID); and assign that to $res (refer to index.php)
+
+$res=$mysqli->query('DELETE FROM `users` WHERE  ID='.$ID);
 
 
  $row = $res->fetch_assoc();
 
 
 
-// do this -> value= "<?= $First_Name;?(space) >"
 
 // if isset POST (refer to personalform.php) do all that then do msqli UPDATE thingy
 
+if(isset($_POST['First_Name'])
+ && (isset($_POST['Last_Name']))
+  && (isset($_POST['Phone']))
+   && (isset($_POST['Username']))
+    && (isset($_POST['Password'])))
 
+    {
+
+     $First_Name= $_POST['First_Name'];
+     $Last_Name= $_POST['Last_Name'];
+     $Phone= $_POST['Phone'];
+     $Username= $_POST['Username'];
+     $Password= $_POST['Password'];
+
+     $mysqli->query(UPDATE `users` SET `ID`=[$ID],`First_Name`=[$First_Name],`Last_Name`=$Last_Name],`Phone`=[$Phone],`Username`=[$Username],`Password`=[Password] WHERE ID=$res')');
+
+   }
 
 
 
@@ -23,9 +45,9 @@
 
 <form action="" method="post">
   <input type="text" name="First_Name" placeholder="First Name" value= "<?= $First_Name;?>">
-  <input type="text" name="Last_Name" placeholder="Last Name" value =>
-  <input type="text" name="Phone" placeholder="Phone Number">
-  <input type="text" name="Username" placeholder="Username">
-  <input type="text" name="Password" placeholder="Password">
+  <input type="text" name="Last_Name" placeholder="Last Name" value ="<?= $Last_Name;?>
+  <input type="text" name="Phone" placeholder="Phone Number" value ="<?= $Phone;?>>
+  <input type="text" name="Username" placeholder="Username" value ="<?= $Username;?>>
+  <input type="text" name="Password" placeholder="Password" value ="<?= $Password;?>>
   <input type="submit">
 </form>
