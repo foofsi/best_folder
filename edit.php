94e8979399a816241e680a0248b07c7226f0fa10
foofsi <34344240+foofsi@users.noreply.github.com>
2018-01-04 19:52:10 +0300

    i

--- edit.php
+++ edit.php
@@ -2,30 +2,18 @@
 
  require_once("connect.php");
 
-// check if the id is set by using $_GET[x], and then assign that to a variable (refer to delete.php)
-
-if(isset($_GET['id']))
-{
-$ID= $_GET['id'];
-}
-
-// $mysqli->query('DELETE FROM `users` WHERE  ID='.$ID); and assign that to $res (refer to index.php)
-
-$res=$mysqli->query('DELETE FROM `users` WHERE  ID='.$ID);
+ if(isset($_GET['id']))
+ {
+ $ID= $_GET['id'];
+ }
 
 
- $row = $res->fetch_assoc();
-
-
-
-
-// if isset POST (refer to personalform.php) do all that then do msqli UPDATE thingy
 
 if(isset($_POST['First_Name'])
- && (isset($_POST['Last_Name']))
-  && (isset($_POST['Phone']))
-   && (isset($_POST['Username']))
-    && (isset($_POST['Password'])))
+ || (isset($_POST['Last_Name']))
+  || (isset($_POST['Phone']))
+   || (isset($_POST['Username']))
+    || (isset($_POST['Password'])))
 
     {
 
@@ -35,19 +23,34 @@ if(isset($_POST['First_Name'])
      $Username= $_POST['Username'];
      $Password= $_POST['Password'];
 
-     $mysqli->query(UPDATE `users` SET `ID`=[$ID],`First_Name`=[$First_Name],`Last_Name`=$Last_Name],`Phone`=[$Phone],`Username`=[$Username],`Password`=[Password] WHERE ID=$res')');
+$sql="UPDATE `users` SET `First_Name`= '".$First_Name."',`Last_Name`= '".$Last_Name."',`Phone`= '".$Phone."',`Username`= '".$Username."',`Password`='".$Password."' WHERE ID=$ID";
+
+
+
+     $mysqli->query($sql);
 
    }
 
+else {
 
+  $res = $mysqli->query("SELECT * FROM users WHERE ID=$ID");
+  $row = $res->fetch_assoc();
+
+  $First_Name= $row['First_Name'];
+  $Last_Name= $row['Last_Name'];
+  $Phone= $row['Phone'];
+  $Username= $row['Username'];
+  $Password= $row['Password'];
+
+}
 
 ?>
 
 <form action="" method="post">
   <input type="text" name="First_Name" placeholder="First Name" value= "<?= $First_Name;?>">
-  <input type="text" name="Last_Name" placeholder="Last Name" value ="<?= $Last_Name;?>
-  <input type="text" name="Phone" placeholder="Phone Number" value ="<?= $Phone;?>>
-  <input type="text" name="Username" placeholder="Username" value ="<?= $Username;?>>
-  <input type="text" name="Password" placeholder="Password" value ="<?= $Password;?>>
+  <input type="text" name="Last_Name" placeholder="Last Name" value ="<?= $Last_Name;?>">
+  <input type="text" name="Phone" placeholder="Phone Number" value ="<?= $Phone;?>">
+  <input type="text" name="Username" placeholder="Username" value ="<?= $Username;?>">
+  <input type="text" name="Password" placeholder="Password" value ="<?= $Password;?>">
   <input type="submit">
 </form>
